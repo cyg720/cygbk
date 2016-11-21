9172009767acbf69face2910af8c04be5c912e7c
cyg <786178187@qq.com>
2016-11-21 19:39:50 +0800

    package change cyg

--- src/test/resources/mytest.sql
+++ src/test/resources/mytest.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50705
 Source Host           : localhost:3306
-Source Database       : mytest
+Source Database       : boke
 
 Target Server Type    : MYSQL
 Target Server Version : 50705
